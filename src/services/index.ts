8d721ffcf1a2f74a1ba5aebdf999ee46a96c6dd7
mimshins <mostafa.sh.coderino@gmail.com>
2021-11-02 15:29:45 +0330

    configure new services

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,8 +1,12 @@
 import type { Application } from "../types.d";
+import passwordManagement from "./password-management/password-management.service";
 import users from "./users/users.service";
+import verifyAccount from "./verify-account/verify-account.service";
 
 const transport = (app: Application) => {
   app.configure(users);
+  app.configure(verifyAccount);
+  app.configure(passwordManagement);
 };
 
 export default transport;
